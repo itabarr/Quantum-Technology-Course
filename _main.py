f5d54f97834f6a2cee35fe36d85b196817c81da6
itabarr <itabarr825@gmail.com>
2023-06-24 17:40:15 +0300

    new files

---
+++ _main.py
@@ -0,0 +1,108 @@
+# import required libraries
+import math
+import matplotlib.pyplot as plt
+from qiskit import execute , Aer
+from qiskit import QuantumCircuit, transpile
+from qiskit.tools.visualization import plot_histogram
+from qiskit_aer.noise import NoiseModel, QuantumError, ReadoutError, pauli_error, depolarizing_error, thermal_relaxation_error
+
+
+simulator = Aer.get_backend('qasm_simulator')
+
+pi = math.pi
+p_meas = 0.1
+
+error_meas = pauli_error([('X', p_meas), ('I', 1 - p_meas)])
+noise_meas = NoiseModel()
+noise_meas.add_all_qubit_quantum_error(error_meas, "measure")
+
+# Create Quantum Fourier Transform circuit with qbits_num qubits
+def create_QFT_circuit(qbits_num = 3):
+    
+    # Create the circuit
+    circuit = QuantumCircuit(3,0)
+
+    # Encode the state |101>
+    circuit.x(0)
+    circuit.x(2)
+
+    # start of QFT
+    circuit.h(2)
+    circuit.cp(pi / 4, 2, 0)
+    circuit.cp(pi / 2, 2, 1)
+    circuit.h(1)
+    circuit.cp(pi / 2, 1, 0)
+    circuit.h(0)
+    circuit.swap(2, 0)
+
+    #end of QFT
+    circuit.barrier()
+    #Start of IQFT
+    circuit.swap(0, 2)
+    circuit.h(0)
+    circuit.cp(-pi / 2, 1, 0)
+    circuit.h(1)
+    circuit.cp(-pi / 2, 2, 1)
+    circuit.cp(-pi / 4, 2, 0)
+    circuit.h(2)
+
+    circuit.measure_all()
+
+
+    return circuit
+
+def excecute_circuit(circuit , shots = 10000):
+
+    # Run ideal simulation
+    job = execute(circuit, simulator, shots=shots)
+    result = job.result()
+    counts = result.get_counts(circuit)
+
+    #Plot ideal
+    plot_histogram(counts)
+    circuit.draw(output='mpl')
+    plt.show()
+
+
+# # add error to one of the X gates
+# p_gate1 = 0.05
+# p_gate2 = 0.35
+# error_gate1 = pauli_error([('X', p_gate1), ('I', 1 - p_gate1)])
+
+# # Add errors to noise model
+# noise_x = NoiseModel()
+# noise_x.add_all_qubit_quantum_error(error_gate1, ["x"])
+
+# print(noise_x)
+
+# job = execute(circuit, simulator,
+#                   basis_gates=noise_x.basis_gates,
+#                   noise_model=noise_x)
+# result = job.result()
+# counts = result.get_counts(circuit)
+
+# # Plot noisy output
+# plot_histogram(counts)
+# plt.show()
+
+
+
+## if we want to see the statevector for debuding
+# sim = Aer.get_backend("aer_simulator")
+circuit_init = circuit.copy()
+circuit_init.save_statevector()
+# statevector = sim.run(circuit_init).result().get_statevector()
+# print('State Vector:', statevector)
+
+
+
+if __name__ == "__main__":
+    
+
+    # create quantum fourier transform circuit
+    circuit = create_QFT_circuit(qbits_num = 3)
+
+    # excecute the circuit
+    excecute_circuit(circuit)
+
+    
--- main.py
+++ main.py
@@ -1,97 +1,199 @@
+from qiskit import QuantumCircuit
+from qiskit import Aer, execute
+from qiskit.visualization import plot_histogram
 
-import math
+import numpy as np
+from math import pi
 import matplotlib.pyplot as plt
-from qiskit import (
-execute,
-Aer)
-simulator = Aer.get_backend('qasm_simulator')
-from qiskit import QuantumCircuit, transpile
-from qiskit.tools.visualization import plot_histogram
-from qiskit_aer.noise import (NoiseModel, QuantumError, ReadoutError,
-    pauli_error, depolarizing_error, thermal_relaxation_error)
+
+def qft_rotations(circuit, n):
+    """Performs qft on the first n qubits in circuit (without swaps)"""
+    if n == 0:
+        return circuit
+    n -= 1
+    circuit.h(n)
+    for qubit in range(n):
+        circuit.cp(pi/2**(n-qubit), qubit, n)
+    # At the end of our function, we call the same function again on
+    # the next qubits (we reduced n by one earlier in the function)
+    qft_rotations(circuit, n)
+
+def swap_registers(circuit, n):
+    """Swapping the qubit registers for QFT operation."""
+    for qubit in range(n//2):
+        circuit.swap(qubit, n-qubit-1)
+    return circuit
+
+def create_qft_circuit(n , initial_state = None):
+    """Create a Quantum Fourier Transform circuit for n qubits."""
+    # Initialize a quantum circuit with n qubits
+    circuit = QuantumCircuit(n)
+
+    if initial_state is not None:
+        # normalize the initial state
+        initial_state = initial_state / np.linalg.norm(initial_state)
+
+        # Check that the length of initial_state is correct
+        if (initial_state.shape[0] != 2**n):
+            raise ValueError("The length of initial_state must equal 2**n_qubits.")
+
+        # Initialize the qubits
+        circuit.initialize(initial_state, range(n))
+
+    # Create the QFT rotations
+    qft_rotations(circuit, n)
+    # Swap the registers for QFT
+    swap_registers(circuit, n)
+    return circuit
+
+def simulate_circuit(circuit , shots , output_state_vector = None , plot = False):
+    """Simulates a quantum circuit with the Qiskit Aer qasm_simulator and returns the result."""
+    # Get the Aer qasm_simulator
+    simulator = Aer.get_backend('qasm_simulator')
+    circ = circuit.copy()
+
+    # Simulate the circuit with the qasm_simulator
+    result = execute(circ, backend=simulator, shots=shots).result()
+
+    # Get the counts (results) from the result object
+    counts = result.get_counts(circ)
+
+    qbits_num = circuit.num_qubits
+
+    # create a list of all possible output states
+    states = [format(i, '0%sb' % qbits_num) for i in range(2**qbits_num)]
+
+    for state in states:
+        if state not in counts:
+            counts[state] = 0  
+    
+    all_counts = np.array([counts.get(state, 0) for state in states])
+    normalized_counts = all_counts / shots
+
+    # Create a bar plot
+
+    if plot:
+        plt.figure()
+        bars = plt.bar(states , normalized_counts)
+        plt.title('Quantum Circuit Simulation Results')
+        plt.xlabel('States')
+        plt.ylabel('Counts')
+        
+        # for bar in bars:
+        #     height = bar.get_height()
+        #     plt.text(bar.get_x() + bar.get_width() / 2, height, str(height), 
+        #             ha='center', va='bottom')
+
+    
+    if output_state_vector is not None:
+        probabilities = np.abs(output_state_vector)**2
+
+        if plot:
+            plt.scatter(states, probabilities, color='red')
+
+    return normalized_counts
+
+def state_vector_simulation(circuit , initial_state = None):
+
+    sim = Aer.get_backend("aer_simulator")
+    state_vector_circuit = circuit.copy()
+
+    if initial_state is not None:
+        # normalize the initial state
+        initial_state = initial_state / np.linalg.norm(initial_state)
+
+        # Check that the length of initial_state is correct
+        if (initial_state.shape[0] != 2**state_vector_circuit.num_qubits):
+            raise ValueError("The length of initial_state must equal 2**n_qubits.")
+
+        plot_state_vector(initial_state, title="Initial state vector")
+    
+    else:
+        initial_state = np.zeros(2**state_vector_circuit.num_qubits)
+        initial_state[0] = 1
+
+        plot_state_vector(initial_state, title="Initial state vector")
+
+    
+    state_vector_circuit.save_statevector()
+    states_vector = sim.run(state_vector_circuit).result().get_statevector()
+
+    plot_state_vector(states_vector, title="Output state vector")
+
+    # Create a list of state labels in binary
+    num_qubits = int(np.log2(np.asarray(states_vector).shape[0]))
+    labels = [format(i, '0' + str(num_qubits) + 'b') for i in range(2**num_qubits)]
+    states = np.asarray(labels)
+
+    states_vector = np.asarray(states_vector)
+
+    return states , states_vector
+
+def plot_state_vector(state_vector, title=""):
+    # Calculate the probabilities from the state vector
+    probabilities = np.abs(state_vector)**2
+
+    # Create a list of state labels in binary
+    num_qubits = int(np.log2(np.asarray(state_vector).shape[0]))
+    labels = [format(i, '0' + str(num_qubits) + 'b') for i in range(2**num_qubits)]
+    
+    # Create a bar plot
+    # new figure
+    plt.figure()
+    plt.bar(labels, probabilities)
+    plt.title(title)
+    plt.xlabel('States')
+    plt.ylabel('Probabilities')
+
+if __name__ == "__main__":
+    n = 5  # Change this to the number of qubits you want
+    
+    initial_state = np.zeros(2**n)
+    initial_state[0] = 0.5
+    initial_state[1] = 0.5
+
+    #normalize the initial state
+    initial_state = initial_state / np.linalg.norm(initial_state)
+
+    qft_circuit = create_qft_circuit(n , initial_state )
+    states , unmeasured_states_vector  = state_vector_simulation(qft_circuit , initial_state )
+
+    #qft_circuit.initialize(initial_state , range(n)) 
+    qft_circuit.measure_all()
+    
+
+    shots = 10000
+    measured_probabilities_vector = simulate_circuit(qft_circuit , shots, unmeasured_states_vector , plot = True)
+    unmeasured_probabilities_vector = np.abs(unmeasured_states_vector)**2
 
 
-pi = math.pi
-p_meas = 0.1
+    # calculate the fidelity between the measured and unmeasured states
+    prob_diff = (measured_probabilities_vector - unmeasured_probabilities_vector)**2
+    total_error = np.sum(prob_diff)
 
-error_meas = pauli_error([('X', p_meas), ('I', 1 - p_meas)])
-noise_meas = NoiseModel()
-noise_meas.add_all_qubit_quantum_error(error_meas, "measure")
+    print("Total Error: ", total_error)
+    
+    # create expnonential range of shots
+    num_of_samples = 10
+    shots_range = np.linspace(1000, 100000, num_of_samples, dtype=int)
+    errors = []
 
+    for i, shots in enumerate(shots_range):
+        if i == num_of_samples - 1:
+            measured_probabilities_vector = simulate_circuit(qft_circuit , shots, unmeasured_states_vector , plot=True)
+        
+        else:
+            measured_probabilities_vector = simulate_circuit(qft_circuit , shots, unmeasured_states_vector)
+        prob_mse = np.sqrt(np.sum((measured_probabilities_vector - unmeasured_probabilities_vector)**2))
+        
+        random_prob_diff = np.ones(len(prob_diff)) * (1/len(prob_diff))
+        random_mse = np.sqrt(np.sum((unmeasured_probabilities_vector - random_prob_diff)**2))
 
-# Create the circuit
-circuit = QuantumCircuit(3,0)
+        total_error = prob_mse / random_mse
+        errors.append(total_error)
 
-
-
-# Encode the state |101>
-circuit.x(0)
-circuit.x(2)
-# start of QFT
-circuit.h(2)
-circuit.cp(pi / 4, 2, 0)
-circuit.cp(pi / 2, 2, 1)
-circuit.h(1)
-circuit.cp(pi / 2, 1, 0)
-circuit.h(0)
-circuit.swap(2, 0)
-#end of QFT
-circuit.barrier()
-#Start of IQFT
-circuit.swap(0, 2)
-circuit.h(0)
-circuit.cp(-pi / 2, 1, 0)
-circuit.h(1)
-circuit.cp(-pi / 2, 2, 1)
-circuit.cp(-pi / 4, 2, 0)
-circuit.h(2)
-
-circuit.measure_all()
-
-
-
-
-# Run ideal simulation
-job = execute(circuit, simulator, shots=10000)
-result = job.result()
-counts = result.get_counts(circuit)
-
-#Plot ideal
-plot_histogram(counts)
-circuit.draw(output='mpl')
-plt.show()
-
-
-# add error to one of the X gates
-p_gate1 = 0.05
-p_gate2 = 0.35
-error_gate1 = pauli_error([('X', p_gate1), ('I', 1 - p_gate1)])
-
-# Add errors to noise model
-noise_x = NoiseModel()
-noise_x.add_all_qubit_quantum_error(error_gate1, ["x"])
-
-print(noise_x)
-
-job = execute(circuit, simulator,
-                  basis_gates=noise_x.basis_gates,
-                  noise_model=noise_x)
-result = job.result()
-counts = result.get_counts(circuit)
-
-# Plot noisy output
-plot_histogram(counts)
-plt.show()
-
-
-
-## if we want to see the statevector for debuding
-# sim = Aer.get_backend("aer_simulator")
-# circuit_init = circuit.copy()
-# circuit_init.save_statevector()
-# statevector = sim.run(circuit_init).result().get_statevector()
-# print('State Vector:', statevector)
-
-
-
-
+        
+        print(f"i: {i} Shots: {shots} Total Error: {total_error}")
+    plt.figure()
+    plt.plot(shots_range , errors)
+    plt.show()
